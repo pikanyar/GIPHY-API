55a72f8bd621051b7f0725e325bc7e21df571974
Pinar <pinarkaya10@hotmail.com>
2019-10-26 20:56:14 -0400

    updated

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 // Initial array of movies
-var movies = ["office", "friends", "Mr. Nobody", "The Lion King"];
+var movies = ["The Godfather", "12 Angry Men", " Pulp Fiction", "The Matrix", "Life Is Beautiful", "The Usual Suspects ", " The Pianist", " Star Wars"];
 
 
 var apikey = "5sAdRPYmPRjCwe9qHB5LstWj4B7mYLAy"
@@ -25,26 +25,34 @@ function displayMovieInfo() {
 
         // Deletes the movies prior to adding new movies
         // (this is necessary otherwise you will have repeat buttons)
-        $("#movies-view").empty(response);
+        $("#movies-view").empty();
+        // $("#movies-view").empty(response);
 
 
         // storing the data from the AJAX request in the results variable
-        var results = response.data;
-
+        var results = response.data;// WHY VAR?????  use let or const
+        console.log(results);
         // Looping through each result item
-        for (var i = 0; i < results.length; i++) {
+        for (let i = 0; i < results.length; i++) {
 
             // Creating and storing a div tag
-            var movieDiv = $("<div>");
+            let movieDiv = $("<div>");
 
             // Creating a paragraph tag with the result item's rating
-            var p = $("<p>").text("Rating: " + results[i].rating);
+            let p = $("<p>").text("Rating: " + results[i].rating);
 
             // Creating and storing an image tag
-            var movieImage = $("<img>");
-            // Setting the src attribute of the image to a property pulled off the result item
-            movieImage.attr("src", results[i].images.fixed_height.url);
+            let movieImage = $("<img>");
+            // movieImage.addClass("gif");
 
+            // movieImage needs data-still and data-animate and data-state
+            // event delegation !!!
+            // Setting the src attribute of the image to a property pulled off the result item
+            movieImage.attr("src", results[i].images.fixed_height_still.url);
+            movieImage.attr("data-still", results[i].images.fixed_height_still.url);
+            movieImage.attr("data-animate", results[i].images.fixed_height.url);
+            movieImage.attr("data-state", "still");
+            movieImage.addClass('movieImage');
             // Appending the paragraph and image tag to the movieDiv
             movieDiv.append(p);
             movieDiv.append(movieImage);
@@ -53,24 +61,41 @@ function displayMovieInfo() {
             $("#movies-view").prepend(movieDiv);
         }
 
-        $("movieImage").on("click", function () {
-            // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-            var state = $(this).attr("data-state");
-            // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-            // Then, set the image's data-state to animate
-            // Else set src to the data-still value
-            if (state === "still") {
-                $(this).attr("src", $(this).attr("data-animate"));
-                $(this).attr("data-state", "animate");
-            } else {
-                $(this).attr("src", $(this).attr("data-still"));
-                $(this).attr("data-state", "still");
-            }
-        });
-
-    });
-
+    })
+    // .then(function () {
+    //     $(".gif").on("click", function () {
+    //         // $("#movies-view").on("click", ".gif", function () {
+    //         // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+    //         var state = $(this).attr("data-state");
+    //         // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+    //         // Then, set the image's data-state to animate
+    //         // Else set src to the data-still value
+    //         if (state === "still") {
+    //             $(this).attr("src", $(this).attr("data-animate"));
+    //             $(this).attr("data-state", "animate");
+    //         } else {
+    //             $(this).attr("src", $(this).attr("data-still"));
+    //             $(this).attr("data-state", "still");
+    //         }
+    //     });
+    // })
 }
+
+// $(".gif").on("click", function () {
+$("#movies-view").on("click", ".movieImage", function () {
+    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+    var state = $(this).attr("data-state");
+    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+    // Then, set the image's data-state to animate
+    // Else set src to the data-still value
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+});
 
 // Function for displaying movie data
 function renderButtons() {
